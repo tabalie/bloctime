f13914325716aaf9997fa16983acce81d37e6a44
Natalie Kueneman <tabalie@gmail.com>
2017-02-17 11:29:57 -0700

    progress on timer

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -13,31 +13,49 @@
 
         // function that counts down 1 from the starting counter time
         var timerCountdown = function(counter) {
-            $scope.counter = $scope.counter - 1;
+            $scope.counter = $scope.counter - 1000;
         };
 
         // need startTimer and stopTimer to toggle between
         //    start and stop/reset timer
 
         // start timer method
-        $scope.startTimer = function() {
+        var startTimer = function() {
+            // interval is in milliseconds
             myInterval = $interval(timerCountdown, 1000);
-            $scope.state = "countingDown";
+            $scope.isCountingDown = true;
             // change button text from "Start" to "Stop"
             $scope.buttonText = "Stop";
         };
 
         // stop timer method
-        $scope.stopTimer = function(myInterval) {
-            // must cancel interval!
-            $interval.cancel(myInterval);
-            $scope.state = "stopped";
+        // make it reusbale for the break as well (depending on state, reset timer, reset 5 min break, reset onger break)
+        // maybe make this reset timer, so add a reset to the counter
+        var stopTimer = function() {
+            // must cancel interval! should log "true" if interval canceled
+            console.log($interval.cancel(myInterval));
+            $scope.isCountingDown = false;
             // change button text from "Stop" to "Start"
             $scope.buttonText = "Start";
         };
 
+        // $scope.toggle =
+        // if state stopped then kjglj
+        // if state countingDown then asdlfsldf
+
+        $scope.toggleTimer = function() {
+            if ($scope.isCountingDown) {
+                stopTimer();
+            } else {
+                startTimer();
+            }
+        };
+
+
         // break
-        // take 5 minute break and then startTimer
+        // listen for a break (in this case, counter = 0)
+        // keep track of completed breaks so i know when to have longer break
+        // take 5 minute break once timer  gets to 0, and then auto start work session startTimer
 
     }
 
